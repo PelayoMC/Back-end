f28cadab30ff61483613716d0c52cae7abaf57a3
Pelayo Martinez <pmartinezcapela@gmail.com>
2020-06-30 01:45:35 +0200

    Cambios para conexión con front

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-module.exports.FRONT_URL = 'http://localhost:4200/';
+module.exports.FRONT_URL = 'https://proyecto-tfg-1582773697712.web.app';
 module.exports.SEED = '@secret@seed--dificult:_SeEED09:';
 // Google
 module.exports.CLIENT_ID = '72467451062-1uu3j42qch0fl30i53881sf6r6ejljsk.apps.googleusercontent.com';
