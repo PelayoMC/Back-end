2d82db1862da3a5e8beafca257389fcc157bc68e
Pelayo Martinez <pmartinezcapela@gmail.com>
2020-06-30 02:36:05 +0200

    Cambio url pruebas

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ var app = express();
 
 // CORS - Control de peticiones
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://proyecto-tfg-1582773697712.web.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+    // res.header("Access-Control-Allow-Origin", "https://proyecto-tfg-1582773697712.web.app"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
